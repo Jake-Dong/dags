440cee669867a76285041b02d4641ad47c8f3d45
Jake-Dong <72381484+Jake-Dong@users.noreply.github.com>
2021-04-06 19:18:18 +0900

    Update New_biblio_cition_dag.py

--- New_biblio_cition_dag.py
+++ New_biblio_cition_dag.py
@@ -10,7 +10,7 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 
 def bibio_citaion_DB():
-    host_ip = '172.29.192.1'
+    host_ip = '10.128.0.21'
     biblio_all_list = []
     yesterday = datetime.today() - timedelta(7)
 
